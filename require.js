3a26433500c66ea9ed9fdbf4aaed54b0c4c17848
Jairaj Sethi <j@jairaj.org>
2011-06-17 00:04:44 -0400

    no more async failing

--- require.js
+++ require.js
@@ -1,6 +1,7 @@
 require = (function(dir, ext){
 	dir = dir || '';
 	ext = ext || '';
+	
 	var cache = {},
 	 	require = function(script_name, callback){
 			// Multiple requires
@@ -33,14 +34,21 @@ require = (function(dir, ext){
 					head = document.getElementsByTagName('head')[0],
 					script = document.createElement('script');
 				script.src = dir+script_name+ext;
+				script.async = true;
 				script.onload = script.onreadystatechange = function(){
 					if(done)
 						return;
 					if (!this.readyState || this.readyState == 'loaded'
 											|| this.readyState == 'complete') {
 						done = true;
-						cache[script_name] = true;
-						callback && callback();
+						
+						if( typeof(cache[script_name]) != 'boolean' ) {
+							cache[script_name].push(callback);
+						} else {
+							cache[script_name] = true;
+							callback && callback();
+						}
+						
 						script.onload = script.onreadystatechange = null;
 						head.removeChild(script);
 					}
@@ -48,5 +56,17 @@ require = (function(dir, ext){
 				head.appendChild(script);
 			}
 		};
+	
+	require.start = function(script_name){
+		cache[script_name] = [];
+	};
+	
+	require.end = function(script_name){
+		var scripts = cache[script_name];
+		cache[script_name] = true;
+		for(var i=0, l=scripts.length; i<l; ++i)
+			scripts[i] && scripts[i]();
+	};
+	
 	return require;
 })();
